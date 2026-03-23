f72b5f28e674d84249e05494fd95ea1b574c028c
lucasdedango <Lucasdedango@gmail.com>
2026-03-23 20:12:24 +0100

    structure update and "profiles" added
    structure update and "profiles" added

---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -profiles=profiles -mod=@CF;@DabsFramework;@DayZ-Expansion-Bundle;@DayZ-Expansion-Licensed;@DayZ-Expansion-Animations;@InfiniteStamina;@Community-Online-Tools;@DeerIsle
+pause
